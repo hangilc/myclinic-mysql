209529d8b9c4ff69f0110d208fe3339aea8afa51
hangilc <hangil@chang.jp>
2023-09-13 09:59:48 +0900

    updated docker-demo

--- docker-demo/build.sh
+++ docker-demo/build.sh
@@ -1,6 +1,7 @@
+# 1.0.4 -- converted app_event data to JSON
 # 1.0.3 -- adapted to migrations upto 06-add-memo-to-conduct-shinryou.sql
 # 1.0.2 -- added memo column to kouhi table
 
-VERSION=1.0.3
+VERSION=1.0.4
 
 docker build -t hangilc/myclinic-mysql-demo:$VERSION .
---
+++ migrations/07-convert-app-event-data-to-json.sql
@@ -0,0 +1,1 @@
+alter table app_event modify column data json not null;
