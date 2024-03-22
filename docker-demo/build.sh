ff72069243767d345f15d93120d623376a63b1fb
hangilc <hangil@chang.jp>
2024-03-23 07:54:50 +0900

    added patient summary

--- docker-demo/build.sh
+++ docker-demo/build.sh
@@ -1,7 +1,8 @@
+# 1.0.5 -- added patient summary
 # 1.0.4 -- converted app_event data to JSON
 # 1.0.3 -- adapted to migrations upto 06-add-memo-to-conduct-shinryou.sql
 # 1.0.2 -- added memo column to kouhi table
 
-VERSION=1.0.4
+VERSION=1.0.5
 
 docker build -t hangilc/myclinic-mysql-demo:$VERSION .
