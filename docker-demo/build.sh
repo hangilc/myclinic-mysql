86a125b48898e447be132a09c11fb91470def11b
hangilc <hangil@chang.jp>
2024-03-23 12:12:44 +0900

    added notice in build.sh

--- docker-demo/build.sh
+++ docker-demo/build.sh
@@ -1,3 +1,5 @@
+# If XX-....sql file has been added, corresponding COPY ... line should be added to Dockerfile.
+
 # 1.0.6 -- added patient summary
 # 1.0.4 -- converted app_event data to JSON
 # 1.0.3 -- adapted to migrations upto 06-add-memo-to-conduct-shinryou.sql
